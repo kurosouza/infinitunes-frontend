3d6c317e76e7995a1ef0e8fe1c90a57176ad851d
Kuro Souza <kurosouza@gmail.com>
2024-10-26 01:23:19 +0200

    NextJS + Grommet setup.

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
-import "@/styles/globals.css";
+import { Grommet, grommet as grommetTheme } from "grommet";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Grommet theme={grommetTheme}>
+      <Component {...pageProps} />
+    </Grommet>
+  );
 }
