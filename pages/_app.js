57b7339e4732937b0b04f3c683142147313a2d08
Kuro Souza <kurosouza@gmail.com>
2024-10-26 12:57:56 +0200

    Changed font - roboto

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,21 @@
 import { Grommet, grommet as grommetTheme } from "grommet";
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({ subsets: ["latin"], weight: ["500", "700"] });
 
 export default function App({ Component, pageProps }) {
   return (
-    <Grommet theme={grommetTheme}>
-      <Component {...pageProps} />
-    </Grommet>
+    <>
+      <style jsx global>
+        {`
+          html {
+            font-family: ${roboto.style.fontFamily};
+          }
+        `}
+      </style>
+      <Grommet theme={grommetTheme}>
+        <Component {...pageProps} />
+      </Grommet>
+    </>
   );
 }
